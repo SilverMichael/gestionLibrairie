f8ffc0bd41911cb0782776debaa787cbf17bf2ed
SilverMichael <emeraudesnake@yahoo.com>
2024-05-21 23:41:04 +0300

    feat: add variables tailwind

--- biblio-front/tailwind.config.js
+++ biblio-front/tailwind.config.js
@@ -7,7 +7,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        blueLight: '#009DE0'
+        "black-100": "#2B2C35",
+        "primary-blue": {
+          DEFAULT: "#2B59FF",
+          100: "#F5F8FF",
+        },
+        "secondary-orange": "#f79761",
+        "light-white": {
+          DEFAULT: "rgba(59,60,152,0.03)",
+          100: "rgba(59,60,152,0.02)",
+        },
+        grey: "#747A88",
+      },
+      backgroundImage: {
+        'landing': "url('/biblio.jpeg')"
       }
     },
   },
