8617b485a8e34b61714e24e6f4a6663281935976
SilverMichael <emeraudesnake@yahoo.com>
2024-05-22 14:27:48 +0300

    fix: typo in started

--- api-user/index.js
+++ api-user/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const cookieParser = require('cookie-parser');
 
 const app = express()
 require('dotenv').config({path: './env'})
 const path = require('path')
 
-
 app.use(cors())
 app.options('*', cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 
 //route
@@ -20,6 +21,6 @@ app.use('/api/user', userRoute)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> {
-    console.log(`server strted on ${PORT}`)
+    console.log(`server started on ${PORT}`)
 })
 
