7069ff5f6b72bba798ca67e31218a23e9107e82c
SilverMichael <emeraudesnake@yahoo.com>
2024-05-22 22:35:31 +0300

    feat: add import custom text area

--- biblio-front/src/components/index.jsx
+++ biblio-front/src/components/index.jsx
@@ -3,11 +3,13 @@ import CustomButton from "./CustomButtons";
 import CustomInput from "./CustomInput";
 import CustomRadio from "./CustomRadio";
 import CustomSelect from "./CustomSelect";
+import CustomTextArea from "./CustomTextArea";
 
 export {
     NavBar, 
     CustomButton, 
     CustomInput, 
     CustomRadio,
-    CustomSelect
+    CustomSelect, 
+    CustomTextArea
 }
