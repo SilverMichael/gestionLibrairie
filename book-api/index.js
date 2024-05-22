0f3dd78317468fda935f19d326ecc379a2322325
SilverMichael <emeraudesnake@yahoo.com>
2024-05-23 00:18:26 +0300

    refactor: rearrange server for multer

--- book-api/index.js
+++ book-api/index.js
@@ -1,22 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
-const app = express()
-require('dotenv').config({path: './env'})
-const path = require('path')
+const app = express();
+require("dotenv").config({ path: "./env" });
 
-app.use(cors())
-app.options('*', cors())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(cors());
+app.options("*", cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//route
-const bookRoute = require('./routes/book.route')
-app.use('/api/book', bookRoute)
 
-const PORT = process.env.PORT || 5001
 
-app.listen(PORT, ()=> {
-    console.log(`server book started on ${PORT}`)
-})
+//routes
+const bookRoute = require("./routes/book.route");
+const uploadRoute = require('./routes/upload.route')
+app.use("/api/book", bookRoute);
+app.use("/api", uploadRoute)
+
+//config static
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+ 
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`server book started on ${PORT}`);
+});
